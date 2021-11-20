cdb609fdce6fe14b9b66019f570919810ab41880
vamseeuxd <vamsee.uxd@gmail.com>
2021-11-20 11:09:05 +0530

    Done with Project Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { IonicModule } from '@ionic/angular';
-import { IonicStorageModule } from '@ionic/storage';
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {InAppBrowser} from '@ionic-native/in-app-browser/ngx';
+import {SplashScreen} from '@ionic-native/splash-screen/ngx';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
+import {IonicModule} from '@ionic/angular';
+import {IonicStorageModule} from '@ionic/storage';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
-import { FormsModule } from '@angular/forms';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {environment} from '../environments/environment';
+import {FormsModule} from '@angular/forms';
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {getFirestore, provideFirestore} from '@angular/fire/firestore';
 
 @NgModule({
   imports: [
@@ -23,10 +25,20 @@ import { FormsModule } from '@angular/forms';
     IonicStorageModule.forRoot(),
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production
-    })
+    }),
+    provideFirebaseApp(() => initializeApp({
+      apiKey: 'AIzaSyDXxViFM-TCNghayNmaTUvulxWA7G6O7fI',
+      authDomain: 'aqua-admins.firebaseapp.com',
+      projectId: 'aqua-admins',
+      storageBucket: 'aqua-admins.appspot.com',
+      messagingSenderId: '118625429177',
+      appId: '1:118625429177:web:be9ce865a5f4144e8cdd68'
+    })),
+    provideFirestore(() => getFirestore()),
   ],
   declarations: [AppComponent],
   providers: [InAppBrowser, SplashScreen, StatusBar],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/app/pages/projects/project-list/add-or-edit-project/add-or-edit-project.component.ts
+++ src/app/pages/projects/project-list/add-or-edit-project/add-or-edit-project.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit, ViewChild} from '@angular/core';
-import {LoadingController, ModalController} from '@ionic/angular';
+import {LoadingController, ModalController, ToastController} from '@ionic/angular';
 import {NgForm} from '@angular/forms';
+import {addDoc, collection, doc, Firestore, updateDoc} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-add-or-edit-project',
@@ -12,10 +13,13 @@ export class AddOrEditProjectComponent implements OnInit {
   @ViewChild('projectForm') projectForm: NgForm;
 
   @Input() isNew = true;
+  @Input() data: any;
 
   constructor(
     public modalController: ModalController,
     public loadingController: LoadingController,
+    public toastController: ToastController,
+    public firestore: Firestore,
   ) {
   }
 
@@ -23,20 +27,71 @@ export class AddOrEditProjectComponent implements OnInit {
   }
 
   ionViewDidEnter() {
-    console.log(this.projectForm);
+    if (this.isNew === false) {
+      this.projectForm.resetForm(this.data);
+    }
+    // console.log(this.projectForm);
   }
 
-  async saveProject() {
+  async saveProject(projectForm: NgForm) {
+    if (this.isNew) {
+      await this.addProject(projectForm);
+    } else {
+      await this.updateProject(projectForm);
+    }
+  }
+
+
+  async updateProject(projectForm: NgForm): Promise<any> {
+    const loading = await this.loadingController.create({
+      message: 'Please wait...',
+      spinner: 'bubbles',
+    });
+    await loading.present();
+    try {
+      const docRef = doc(this.firestore, `projects/${this.data.id}`);
+      await updateDoc(docRef, projectForm.value);
+      await loading.dismiss();
+      const toast = await this.toastController.create({
+        message: 'Project Updated Successfully',
+        duration: 2000
+      });
+      await toast.present();
+      projectForm.resetForm({});
+      await this.modalController.dismiss();
+    } catch (e) {
+
+      const toast = await this.toastController.create({
+        message: 'Technical Error while Updating the Project',
+        duration: 2000
+      });
+      await toast.present();
+      await loading.dismiss();
+    }
+  }
+
+  async addProject(projectForm: NgForm): Promise<any> {
     const loading = await this.loadingController.create({
       cssClass: 'my-custom-class',
       message: 'Please wait...',
     });
     await loading.present();
+    try {
+      await addDoc(collection(this.firestore, 'projects'), projectForm.value);
+      projectForm.resetForm({});
+      await this.modalController.dismiss();
+      await loading.dismiss();
+    } catch (e) {
+      const toast = await this.toastController.create({
+        message: 'Technical Error while Adding the Project',
+        duration: 2000
+      });
+      await toast.present();
+      await loading.dismiss();
+    }
   }
 
   dismiss() {
-    // using the injected ModalController this page
-    // can "dismiss" itself and optionally pass back data
     this.modalController.dismiss({
       dismissed: true,
     });
